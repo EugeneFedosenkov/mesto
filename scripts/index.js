bfc61db09e13243064632c2b2d64c199895849a9
Eugene Fedosenkov <timontim88@yandex.ru>
2022-11-12 03:25:08 +0300

    foteen

--- scripts/index.js
+++ scripts/index.js
@@ -7,10 +7,10 @@ let job = document.querySelector('.profile__job');
 let editButtom = document.querySelector('.profile__edit-buttom');
 
 // переменная кнопки закрытия
-let closeButtom = document.querySelector('.form__closeIcon');
+let closeButtom = document.querySelector('.popup__closeicon');
 
 // переменная попапа которому будем добавлять класс popup_opened
-let popup = document.querySelector('#popup');
+let popup = document.querySelector('.popup');
 //функция открытия popup c добавление значений в value
 function popupOpen(){
 	popup.classList.add('popup_opened');
@@ -35,30 +35,29 @@ let jobInput = formElement.querySelector('.form__item_el_about');// Воспол
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    evt.preventDefault();
+ // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
-nameInput.getAttribute('value');   // Получите значение полей jobInput и nameInput из свойства value
-jobInput.getAttribute('value');
+   // Получите значение полей jobInput и nameInput из свойства value
 
     // Выберите элементы, куда должны быть вставлены значения полей
 
  name.textContent = nameInput.value;   // Вставьте новые значения с помощью textContent
  job.textContent = jobInput.value;
+ popupClose();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
 // Закрытие форма на кнопку Сохранить
-let submit = document.querySelector('.form__button');
-submit.addEventListener('click', popupClose);
 
-// Функция лайка
-let like = document.querySelector('.element__like');
+// Функция лайка пока не нужна
+//let like = document.querySelector('.element__like');
 
-function likeUp(){
-	like.classList.toggle('element__like_active');
-}
+//function likeUp(){
+//	like.classList.toggle('element__like_active');
+//}
 
-like.addEventListener('click', likeUp);
+//like.addEventListener('click', likeUp);
