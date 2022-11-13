e789ab12acb545d003bc6e3377deb01f3e6c9f80
Eugene Fedosenkov <timontim88@yandex.ru>
2022-11-13 03:11:54 +0300

    sixteene

--- scripts/index.js
+++ scripts/index.js
@@ -1,56 +1,53 @@
-
-
-let name = document.querySelector('.profile__name');
-let job = document.querySelector('.profile__job');
+let name = document.querySelector('.profile__name')
+let job = document.querySelector('.profile__job')
 
 // переменная кнопки редактирования
-let editButtom = document.querySelector('.profile__edit-buttom');
+let editButtom = document.querySelector('.profile__edit-buttom')
 
 // переменная кнопки закрытия
-let closeButtom = document.querySelector('.popup__closeicon');
+let closeButtom = document.querySelector('.popup__close-icon')
 
 // переменная попапа которому будем добавлять класс popup_opened
-let popup = document.querySelector('.popup');
+let popup = document.querySelector('.popup')
 //функция открытия popup c добавление значений в value
-function popupOpen(){
-	popup.classList.add('popup_opened');
-    nameInput.value = name.textContent;
-    jobInput.value = job.textContent;
+function popupOpen() {
+  popup.classList.add('popup_opened')
+  nameInput.value = name.textContent
+  jobInput.value = job.textContent
 }
 //функция закрытия popup
-function popupClose(){
-	popup.classList.remove('popup_opened');
+function popupClose() {
+  popup.classList.remove('popup_opened')
 }
 
 // реакция на действие при клике на editButtom происходит функция  popupOpen
-editButtom.addEventListener('click', popupOpen);
-closeButtom.addEventListener('click', popupClose);
+editButtom.addEventListener('click', popupOpen)
+closeButtom.addEventListener('click', popupClose)
 
-
-            // Находим форму в DOM
-let formElement = document.querySelector('.form');// Воспользуйтесь методом querySelector()
+// Находим форму в DOM
+let formElement = document.querySelector('.form') // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector('.form__item_el_name');// Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector('.form__item_el_about');// Воспользуйтесь инструментом .querySelector()
+let nameInput = formElement.querySelector('.form__item_el_name') // Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector('.form__item_el_about') // Воспользуйтесь инструментом .querySelector()
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault();
- // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-   // Получите значение полей jobInput и nameInput из свойства value
+function formSubmitHandler(evt) {
+  evt.preventDefault()
+  // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
+  // Получите значение полей jobInput и nameInput из свойства value
 
-    // Выберите элементы, куда должны быть вставлены значения полей
+  // Выберите элементы, куда должны быть вставлены значения полей
 
- name.textContent = nameInput.value;   // Вставьте новые значения с помощью textContent
- job.textContent = jobInput.value;
- popupClose();
+  name.textContent = nameInput.value // Вставьте новые значения с помощью textContent
+  job.textContent = jobInput.value
+  popupClose()
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', formSubmitHandler)
 // Закрытие форма на кнопку Сохранить
 
 // Функция лайка пока не нужна
