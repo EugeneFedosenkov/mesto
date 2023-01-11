0285632e4533d5793d9798f865779703481cb8c3
Eugene Fedosenkov <timontim88@yandex.ru>
2023-01-12 00:58:49 +0300

    thryfour

--- scripts/constants.js
+++ scripts/constants.js
@@ -2,7 +2,7 @@ const validationSettings = {
   formSelector: '.form',
   inputSelector: '.form__item',
   submitButtonSelector: '.form__button',
-  inactiveButtonClass: 'button_inactive',
+  inactiveButtonClass: 'form__button_inactive',
   inputErrorClass: 'form__item_type_error',
   errorClass: 'form__item-error_active',
 };
